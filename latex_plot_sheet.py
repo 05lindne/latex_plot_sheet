318488cae6c3117d0558f57fa019c71f0504e4fd
05lindne <sarahkoala@live.com>
2015-03-09 17:23:27 +0100

    up to 3x6 plots per page

--- latex_plot_sheet.py
+++ latex_plot_sheet.py
@@ -2,7 +2,7 @@
 
 """ File: latex_plot_sheet.py
 	Author: Sarah Lindner
-	Date of last change: 03.03.2015
+	Date of last change: 09.03.2015
 
 	Takes figure files (pdf, png) in a directory as input and creates a latex file with rows of 3 figures which can be input to a latex file.
 """
@@ -17,21 +17,25 @@ out_file = open(out_filename, 'w')
 # empty file
 out_file.truncate()
 
-index = 0
 
-for index in range(0, len(in_filenames), 3):
-	out_file.write("\\begin{figure}[!ht]\n")
-
-	for i in range(0,3):
-		out_file.write("\\begin{subfigure}[t]{0.29\linewidth}\n")
-		out_file.write("\\begin{center}\n")
-		out_file.write("\\testbox{\includegraphics[width= \\textwidth, clip=true, trim = 0 0 0 0]{" + in_filenames[index + i] + "}}\n")
-		out_file.write("\end{center}\n")
-		out_file.write("\end{subfigure}\n")
-		out_file.write("\hfill\n")
+out_file.write("\\begin{figure}[!t]\n")
 
 
-	out_file.write("\end{figure}\n")
+for index, filename in enumerate(in_filenames):
+	out_file.write("\\begin{subfigure}[t]{0.29\linewidth}\n")
+	out_file.write("\\begin{center}\n")
+	out_file.write("\\testbox{\includegraphics[width= \\textwidth, trim = 0 0 0 0]{" + filename + "}}\n")
+	out_file.write("\end{center}\n")
+	out_file.write("\end{subfigure}\n")
+	out_file.write("\hfill\n")
+
+
+	# stop subfigure after every 3rd file or after last file in list
+	if ((index+1) - len(in_filenames) == 0):
+		out_file.write("\end{figure}\n")
+	elif (index != 0 ) and ( (index+1)%3 == 0):
+		out_file.write("\end{figure}\n")
+		out_file.write("\\begin{figure}[!t]\n")
 
 
 
