8a51d31ac10fbce1868cdc74aef0590c54cdbeef
05lindne <sarahkoala@live.com>
2015-05-19 10:38:26 +0200

    check for empty file

--- latex_plot_sheet_half.py
+++ latex_plot_sheet_half.py
@@ -10,47 +10,53 @@
 from sys import argv 
 import os
 
-out_filename = argv[1]
-in_filenames = argv[2:]
+def main():
+	out_filename = argv[1]
+	in_filenames = argv[2:]
 
-out_file = open(out_filename, 'w')
+	# don't overwrite existing file
+	if os.stat(path_filename(out_filename)).st_size > 0:
+		response = raw_input("Overwrite " + out_filename + " ? - y/n\n")
+		if response.lower().startswith("n"):
+			print("Sayoonara")
+			exit()
 
-# don't overwrite existing file
-if os.stat("out_file").st_size != 0:
-	response = raw_input("Overwrite " + out_filename + " ? - y/n")
-	if response.lower().startswith("n"):
-		print("Sayoonara")
-		exit()
+	out_file = open(out_filename, 'w')
 
-# empty file
-out_file.truncate()
+	# empty file
+	out_file.truncate()
 
-out_file.write("\\begin{figure}[!t]\n")
-
-
-for index, item in enumerate(in_filenames):
-
-	filename = os.path.basename( item )
-	file_directory = os.path.dirname( os.path.realpath( item ) )
-	cwd_directory = os.getcwd()
-
-	out_file.write("\\begin{subfigure}[t]{0.49\linewidth}\n")
-	out_file.write("\\begin{center}\n")
-	out_file.write("\\testbox{\includegraphics[width= \\textwidth, trim = 0 0 0 0]{" + file_directory + "/" + filename + "}}\n")
-	out_file.write("\end{center}\n")
-	out_file.write("\end{subfigure}\n")
+	out_file.write("\\begin{figure}[!t]\n")
 
 
-	# stop subfigure after every 2nd file or after last file in list
-	if ((index+1) - len(in_filenames) == 0):
-		out_file.write("\end{figure}\n")
-		out_file.write("\hfill\n")
-	elif (index != 0 ) and ( (index+1)%2 == 0):
-		out_file.write("\end{figure}\n")
-		out_file.write("\hfill\n")
-		out_file.write("\\begin{figure}[!t]\n")
+	for index, item in enumerate(in_filenames):
 
-out_file.write("\\hfill\n")
-out_file.write("\\vfill\n")
+		filename = path_filename(item)
 
-out_file.close()
+		out_file.write("\\begin{subfigure}[t]{0.49\linewidth}\n")
+		out_file.write("\\begin{center}\n")
+		out_file.write("\\testbox{\includegraphics[width= \\textwidth, trim = 0 0 0 0]{" + file_directory + "/" + filename + "}}\n")
+		out_file.write("\end{center}\n")
+		out_file.write("\end{subfigure}\n")
+
+
+		# stop subfigure after every 2nd file or after last file in list
+		if ((index+1) - len(in_filenames) == 0):
+			out_file.write("\end{figure}\n")
+			out_file.write("\hfill\n")
+		elif (index != 0 ) and ( (index+1)%2 == 0):
+			out_file.write("\end{figure}\n")
+			out_file.write("\hfill\n")
+			out_file.write("\\begin{figure}[!t]\n")
+
+	out_file.write("\\hfill\n")
+	out_file.write("\\vfill\n")
+
+	out_file.close()
+
+def path_filename( file ):
+   return os.path.dirname( os.path.realpath( file ) ) + "/" + os.path.basename( file )
+
+
+if __name__ == '__main__':
+    main()
--- latex_plot_sheet_third.py
+++ latex_plot_sheet_third.py
@@ -15,16 +15,15 @@ def main():
 	out_filename = argv[1]
 	in_filenames = argv[2:]
 
-	out_file = open(out_filename, 'w')
-
 	# don't overwrite existing file
-	# whole_out_filename = 
-	# if os.stat("out_file").st_size != 0:
-	# 	response = raw_input("Overwrite " + out_filename + " ? - y/n")
-	# 	if response.lower().startswith("n"):
-	# 		print("Sayoonara")
-	# 		exit()
+	if os.stat(dir_filename(out_filename)).st_size > 0:
+		response = raw_input("Overwrite " + out_filename + " ? - y/n\n")
+		if response.lower().startswith("n"):
+			print("Sayoonara")
+			exit()
 
+
+	out_file = open(out_filename, 'w')
 
 	# empty file
 	out_file.truncate()
@@ -35,11 +34,10 @@ def main():
 	for index, item in enumerate(in_filenames):
 
 		filename = dir_filename(item)
-		print filename
+
 		out_file.write("\\begin{subfigure}[t]{0.29\linewidth}\n")
 		out_file.write("\\begin{center}\n")
 		out_file.write("\\testbox{\includegraphics[width= \\textwidth, trim = 0 0 0 0]{" + filename + "}}\n")
-		# out_file.write("\\testbox{\includegraphics[width= \\textwidth, trim = 0 0 0 0]{" + file_directory + "/" + filename + "}}\n")
 		out_file.write("\end{center}\n")
 		out_file.write("\end{subfigure}\n")
 
